4625a3cf19164bcb3a91aa4ab28776d5fa5a661c
Alex <oleksii.pidhornyi@gmail.com>
2025-01-31 19:55:16 +0100

    reversed content view

---
+++ RecognizingGestures/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  RecognizingGestures
+//
+//  Created by Oleksii on 31.01.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContentView()
+}
