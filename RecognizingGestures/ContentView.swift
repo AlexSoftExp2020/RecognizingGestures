ae38b33e804841921443110ecd478afe9eff4826
Alex <oleksii.pidhornyi@gmail.com>
2025-01-31 23:08:08 +0100

    add navigation link to list to content view

--- RecognizingGestures/ContentView.swift
+++ RecognizingGestures/ContentView.swift
@@ -9,7 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationSplitView {
+            List {
+                NavigationLink(destination: //MARK: TODO TapView(), label: <#T##() -> View#>)
+            }
+            .navigationTitle("Gestures")
+        } detail: {
+            Text()
+        }
+
     }
 }
 
