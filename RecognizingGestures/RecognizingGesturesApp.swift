4c9cb5f475b7a5ac2cf69b9b47c79f426a202fa1
Alex <oleksii.pidhornyi@gmail.com>
2025-01-31 19:49:36 +0100

    Initial Commit

---
+++ RecognizingGestures/RecognizingGesturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecognizingGesturesApp.swift
+//  RecognizingGestures
+//
+//  Created by Oleksii on 31.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct RecognizingGesturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
