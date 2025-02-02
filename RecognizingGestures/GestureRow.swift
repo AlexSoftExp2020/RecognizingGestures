676fa9d5ee3d22bf606cead3f23bf5061f80e680
Alex <oleksii.pidhornyi@gmail.com>
2025-02-02 20:21:00 +0100

    add gesture row view

---
+++ RecognizingGestures/GestureRow.swift
@@ -0,0 +1,18 @@
+//
+//  GestureRow.swift
+//  RecognizingGestures
+//
+//  Created by Oleksii on 02.02.2025.
+//
+
+import SwiftUI
+
+struct GestureRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GestureRow()
+}
