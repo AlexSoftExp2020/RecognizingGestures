0350bcab3301da6a92ab97d4abde59c875e3c2ba
Alex <oleksii.pidhornyi@gmail.com>
2025-02-02 19:58:52 +0100

    add tap gesture to tap view

--- RecognizingGestures/TapView.swift
+++ RecognizingGestures/TapView.swift
@@ -8,6 +8,16 @@
 import SwiftUI
 
 struct TapView: View {
+    @State private var color: Color = Color.primary
+    
+    var tapGesture: some Gesture {
+        TapGesture()
+            .onEnded {
+            withAnimation {
+                color = Color.random()
+            }
+        }
+    }
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
