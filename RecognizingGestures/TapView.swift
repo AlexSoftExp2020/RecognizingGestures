b28d059e6a0be776ea7227171afa6e61596dc7e3
Alex <oleksii.pidhornyi@gmail.com>
2025-02-02 20:05:06 +0100

    add color extension

--- RecognizingGestures/TapView.swift
+++ RecognizingGestures/TapView.swift
@@ -26,3 +26,11 @@ struct TapView: View {
 #Preview {
     TapView()
 }
+
+extension Color {
+    static func random() -> Color {
+        return Color(red: Double.random(in: 0...1),
+                     green: Double.random(in: 0...1),
+                     blue: Double.random(in: 0...1))
+    }
+}
