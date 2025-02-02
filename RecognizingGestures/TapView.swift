fdb880ed6800d14f29bce86979a0767711d1b7a1
Alex <oleksii.pidhornyi@gmail.com>
2025-02-02 19:46:57 +0100

    add tap view

---
+++ RecognizingGestures/TapView.swift
@@ -0,0 +1,18 @@
+//
+//  TapView.swift
+//  RecognizingGestures
+//
+//  Created by Oleksii on 02.02.2025.
+//
+
+import SwiftUI
+
+struct TapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TapView()
+}
