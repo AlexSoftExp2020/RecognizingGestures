494e2c8131c97c04051f372e22236a47f06fa149
Alex <oleksii.pidhornyi@gmail.com>
2025-02-04 13:05:04 +0100

    add drag view

---
+++ RecognizingGestures/DragView.swift
@@ -0,0 +1,18 @@
+//
+//  DragView.swift
+//  RecognizingGestures
+//
+//  Created by Oleksii on 04.02.2025.
+//
+
+import SwiftUI
+
+struct DragView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DragView()
+}
