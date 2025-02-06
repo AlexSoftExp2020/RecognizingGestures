5f350deb8d275d64d06445cec75b0437a65961ec
Alex <oleksii.pidhornyi@gmail.com>
2025-02-06 20:41:24 +0100

    extend single view

--- RecognizingGestures/SingleView.swift
+++ RecognizingGestures/SingleView.swift
@@ -8,8 +8,40 @@
 import SwiftUI
 
 struct SingleView: View {
+    @State var lineStart = CGPoint.zero
+    @State var lineEnd = CGPoint.zero
+    
+    var lineDrawingGesture: some Gesture {
+        DragGesture()
+            .onChanged { value in
+            lineStart = value.startLocation
+            lineEnd = value.location
+        }
+            .onEnded { value in
+                lineEnd = value.location
+            }
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Touch and drag to make a line")
+            Spacer()
+            Path { path in
+                path.move(to: lineStart)
+                path.move(to: lineEnd)
+            }
+            .stroke(Color.green, lineWidth: 8.0)
+            .contentShape(Rectangle())
+            .gesture(lineDrawingGesture)
+        }
+        .navigationTitle("Line Drawing")
+        .padding()
+        .toolbar {
+            Button("Reset") {
+                lineStart = .zero
+                lineEnd = .zero
+            }
+        }
     }
 }
 
