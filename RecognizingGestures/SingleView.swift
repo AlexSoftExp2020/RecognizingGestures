7d78edffe84903f5feda6c63ac8958b07dd2106c
Alex <oleksii.pidhornyi@gmail.com>
2025-02-06 18:49:10 +0100

    add single view

---
+++ RecognizingGestures/SingleView.swift
@@ -0,0 +1,18 @@
+//
+//  SingleView.swift
+//  RecognizingGestures
+//
+//  Created by Oleksii on 06.02.2025.
+//
+
+import SwiftUI
+
+struct SingleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SingleView()
+}
